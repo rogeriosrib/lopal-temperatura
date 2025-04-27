adcb1a928209649c2f773ca6ac4a039c2f836100
rogeriosrib <cjo.rogerribeiro@gmail.com>
2025-04-27 17:42:36 -0300

    Organizando leiaute

--- src/br/dev/roger/temperatura/gui/TelaConversor.java
+++ src/br/dev/roger/temperatura/gui/TelaConversor.java
@@ -1,6 +1,7 @@
 package br.dev.roger.temperatura.gui;
 
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,13 +17,15 @@ public class TelaConversor {
 	private JButton buttonFahreinheit;
 	private JLabel labelResultado;
 	private JLabel labelMensagemErro;
+	
+	private Font labelFont = new Font("Arial", Font.BOLD, 13);
 
 	public void criarTela() {
 
 		JFrame tela = new JFrame();
 
 		Dimension tamanho = new Dimension();
-		tamanho.setSize(460, 400);
+		tamanho.setSize(460, 350);
 		tela.setSize(tamanho);
 		tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		tela.setTitle("Conversor de Temperatura");
@@ -31,14 +34,25 @@ public class TelaConversor {
 		tela.setResizable(false);
 		
 		textCelsius = new JTextField();
-		textCelsius.setBounds(30, 80, 385, 35);
+		textCelsius.setBounds(30, 50, 380, 35);
 
 		labelCelsius = new JLabel();
+		labelCelsius.setFont(labelFont);
 		labelCelsius.setText("Temperatura em graus celsius : ");
-		labelCelsius.setBounds(30, 50, 200, 35);
+		labelCelsius.setBounds(30, 20, 250, 35);
+		
+		buttonKelvin = new JButton();
+		buttonKelvin.setText("Kelvin");
+		buttonKelvin.setBounds(230, 100, 180, 40);;
+		
+		buttonFahreinheit = new JButton();
+		buttonFahreinheit.setText("Fahreinheit");
+		buttonFahreinheit.setBounds(30, 100, 180, 40);;
 		
 		tela.getContentPane().add(textCelsius);
 		tela.getContentPane().add(labelCelsius);
+		tela.getContentPane().add(buttonKelvin);
+		tela.getContentPane().add(buttonFahreinheit);
 		
 		tela.setVisible(true);
 	}
